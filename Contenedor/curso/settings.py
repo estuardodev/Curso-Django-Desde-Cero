422152e9cb35564f1c6e8d724cc0985a167425ac
Estuardo Ramírez <estuardito_9@hotmail.com>
2023-08-04 12:48:28 -0600

    7.Creación de Aplicaciones | Curso de Django desde Cero

--- Contenedor/curso/settings.py
+++ Contenedor/curso/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Nuestras aplicaciones
+    'encuestas.apps.EncuestasConfig',
 ]
 
 MIDDLEWARE = [
